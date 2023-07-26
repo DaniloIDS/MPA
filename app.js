e711b0ab939bd075d18adf60e748b672de0d53f0
DaniloIDS <danilo.ids@outlook.com>
2023-07-26 06:35:57 -0300

    Teste de ajuste para deploy

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ const app = express()
 mongoose.connect(process.env.DB_URL)
 const db = mongoose.connection
 
-db.once('open', ()=>{console.log(`Servidor conectado ao banco de dados!`)})
-db.on('error', (err)=>{console.log(`Erro na conexão com o banco de dados \n ${err}`)})
-
-
 app.listen(process.env.PORTA, ()=>{console.log('Servidor rodando')})
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'templates'))
 
 app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/users', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req,res)=>{res.render('index')})
 app.use('/users', userRoutes)
+
+db.once('open', ()=>{console.log(`Servidor conectado ao banco de dados!`)})
+db.on('error', (err)=>{console.log(`Erro na conexão com o banco de dados \n ${err}`)})
