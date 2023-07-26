75c29f835ff768435c481950ff365697dd772091
DaniloIDS <danilo.ids@outlook.com>
2023-07-26 06:53:17 -0300

    Teste de ajuste para deploy 3

--- app.js
+++ app.js
@@ -5,23 +5,22 @@ const mongoose = require('mongoose')
 const userRoutes = require('./routes/users')
 const app = express()
 
-
-mongoose.connect(process.env.DB_URL)
-const db = mongoose.connection
-
-app.listen(process.env.PORTA, ()=>{console.log('Servidor rodando')})
-
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'templates'))
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/users', express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req,res)=>{
-    console.log('index')
-    res.send('hello wolrd')})
+app.use('/', (req, res)=>{
+    res.status(200).end('hello world')
+})
 
 app.use('/users', userRoutes)
+
+app.listen(process.env.PORTA, ()=>{console.log('Servidor rodando')})
+
+mongoose.connect(process.env.DB_URL)
+const db = mongoose.connection
 
 db.once('open', ()=>{console.log(`Servidor conectado ao banco de dados!`)})
 db.on('error', (err)=>{console.log(`Erro na conexão com o banco de dados \n ${err}`)})
