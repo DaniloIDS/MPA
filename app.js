ce50d687d7206036ec0670cb32e8c92bd8444421
DaniloIDS <danilo.ids@outlook.com>
2023-07-26 07:11:25 -0300

    Teste de ajuste para deploy 4

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ app.set('views', path.join(__dirname, 'templates'))
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/users', express.static(path.join(__dirname, 'public')))
 
-app.use('/', (req, res)=>{
+app.get('/', (req, res)=>{
     res.status(200).end('hello world')
 })
 
 app.use('/users', userRoutes)
 
-app.listen(process.env.PORTA, ()=>{console.log('Servidor rodando')})
+app.listen(process.env.PORTA, ()=>{console.log('Servidor rodando na porta ' + process.env.PORTA)})
 
-mongoose.connect(process.env.DB_URL)
-const db = mongoose.connection
+// mongoose.connect(process.env.DB_URL)
+// const db = mongoose.connection
 
-db.once('open', ()=>{console.log(`Servidor conectado ao banco de dados!`)})
-db.on('error', (err)=>{console.log(`Erro na conexão com o banco de dados \n ${err}`)})
+// db.once('open', ()=>{console.log(`Servidor conectado ao banco de dados!`)})
+// db.on('error', (err)=>{console.log(`Erro na conexão com o banco de dados \n ${err}`)})
